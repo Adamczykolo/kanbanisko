8af55246017ac8da7b8ba4a9fec8dc5362074a16
Adamczykolo <zcasqe2@gmail.com>
2024-11-18 17:38:21 +0100

    udalo sie kurwa

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require("express")
+const fs = require('fs');
+
 var app = express()
 app.get("/", function (request, response) {
-    response.send(index.html)
+    fs.readFile('index.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            response.write("file not found!");
+        }
+        else {
+            response.write(data);
+        }
+    })
 })
 app.listen(10000, function () {
     console.log("Started application on port %d", 10000)
