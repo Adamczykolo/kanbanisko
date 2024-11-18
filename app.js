a295d2ab142a9b6373f2642807db29858bd3aacb
Adamczykolo <zcasqe2@gmail.com>
2024-11-18 18:25:31 +0100

    wtf??

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-const express = require("express")
+const express = require("express");
 const fs = require('fs');
+const { Sequelize } = require('sequelize');
+require("dotenv").config();
 
 var app = express()
+
+const sequelize = new Sequelize(
+    process.env.DB_URL,
+    {
+        dialect: "postgres",
+        protocol: "postgres",
+        dialectOptions: {
+            ssl: {
+                requrie: true,
+                rejectUnauthorized: false,
+            },
+        },
+        logging: false,
+    });
+
+sequelize.sync().then(() => { console.log("Database connected") }).catch((err) => { console.log(err) });
+
 app.get("/", function (request, response) {
     fs.readFile('index.html', null, function (error, data) {
         if (error) {
