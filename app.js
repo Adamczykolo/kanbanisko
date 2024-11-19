c12f342dd375697332703debe59b5560dbe3b96a
Adamczyko <zcasqe2@gmail.com>
2024-11-19 18:54:56 +0100

    czyta index

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 const { Client } = require('pg')
+var fs = require('fs');
+var http = require('http');
+
+function onRequest(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.readFile('./index.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            response.write('File not found');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    })
+
+}
+
+http.createServer(onRequest).listen(3000);
 
 const client = new Client({
     host: "localhost",
@@ -19,3 +37,5 @@ client.query(`SELECT * FROM users`, (err, res) => {
     }
     client.end;
 })
+
+
