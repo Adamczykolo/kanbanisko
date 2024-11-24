abcc1e19c1b68277e48a8d4d49187d1500ef3524
Adamczyko <zcasqe2@gmail.com>
2024-11-24 20:47:47 +0100

    logowanie jest, to do powitanie

--- index.php
+++ index.php
@@ -1,15 +1,38 @@
+<?php
+    session_start();
+    if(isset($_POST['login']) && isset($_POST['pass']) ){
+        $login = $_POST['login'];
+        $pass = $_POST['pass'];
+        
+        $polaczenie = new mysqli('localhost', username: 'root', '', 'users');
 
+        $query = "SELECT login FROM uzytkownicy WHERE pass LIKE '$pass' AND login LIKE '$login'";
+        
+        
+        $wynik = $polaczenie->query($query);
+        if($wynik->num_rows==1){
+            $_SESSION['logowanie']=$login;
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+        }else{
+            echo "Błędny login lub hasło";
+        }
+    }
+
+    
+
+    if(isset($_SESSION['login'])){
+        echo "jesteś zalogowany";
+        
+    }else {
+        ?>
+        <form action="logowaniebaza.php" method="post">
+    <input type="text" name= "login">
+    <input type="password" name= "pass">
+    <input type="submit" name= "zaloguj">
+    </form>
     <?php
-    echo "halo";
-    ?>
-</body>
-</html>
+    }
+
+?>
+
+
