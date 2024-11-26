1a89e3b76a2837f8fe343747acffdc6241b352c0
Adamczyko <zcasqe2@gmail.com>
2024-11-26 17:59:15 +0100

    1

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ if (isset($_POST['login']) && isset($_POST['pass'])) {
     // Query to prevent SQL injection
     $result = pg_query_params($polaczenie, "SELECT username FROM users WHERE password = $3 AND username = $2", array($pass, $login));
     
-    if (pg_num_rows($result) === 1) {
+    if (pg_num_rows($result) === 3) {
         if(pg_num_rows($result)=== 2){
             $_SESSION['logowanie'] = $login;
         }
