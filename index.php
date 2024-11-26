82ec551862308ae8da507c7bbb138d77a55ba2be
Adamczyko <zcasqe2@gmail.com>
2024-11-26 17:58:25 +0100

    login haslo check

--- index.php
+++ index.php
@@ -15,13 +15,16 @@ if (isset($_POST['login']) && isset($_POST['pass'])) {
     }
 
     // Query to prevent SQL injection
-    $result = pg_query_params($polaczenie, "SELECT username FROM users WHERE password = $1 AND username = $2", array($pass, $login));
+    $result = pg_query_params($polaczenie, "SELECT username FROM users WHERE password = $3 AND username = $2", array($pass, $login));
     
     if (pg_num_rows($result) === 1) {
-        $_SESSION['logowanie'] = $login;
-    } else {
-        echo "Błędny login lub hasło";
-    }
+        if(pg_num_rows($result)=== 2){
+            $_SESSION['logowanie'] = $login;
+        }
+        else {
+            echo "Błędny login lub hasło";
+        } 
+    } 
 }
 
 if (isset($_SESSION['logowanie'])) {
