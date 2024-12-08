b17818e5ac0d8862cd1deacacd2561c20a377eba
Damian Adamczyk <zcasqe2@gmail.com>
2024-12-08 09:36:42 +0100

    helo

--- index.php
+++ index.php
@@ -97,7 +97,7 @@ session_start();
             <div class="greeting">Hello, <?php echo htmlspecialchars($_SESSION['logowanie']); ?>!</div>
             <form method="POST" action="index.php">
                 <input type="hidden" name="logout" value="1">
-                <button type="submit">Log Out</button>
+                <button name="logout" type="submit">Log Out</button>
             </form>
         </div>
         <?php
@@ -158,6 +158,11 @@ session_start();
             </form>
         </div>
     <?php
+    } if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['logout'])) {
+        session_unset();
+        session_destroy();
+        header("Location: index.php");
+        exit;
     }
     ?>
 </body>
